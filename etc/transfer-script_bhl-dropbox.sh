dc7766db3a399ea835786debcc53b5918f5f313d
Max Eckard <eckardm@umich.edu>
2017-01-19 14:39:20 -0500

    threehearted info

--- etc/transfer-script_bhl-dropbox.sh
+++ etc/transfer-script_bhl-dropbox.sh
@@ -2,4 +2,4 @@
 # transfer script example
 # /etc/archivematica/automation-tools/transfer-script.sh
 cd /usr/lib/archivematica/automation-tools/
-/usr/share/python/automation-tools/bin/python -m transfers.transfer --user eckardm --api-key 77cd1a5b8e3cf306261322da376420f141a0c05a --ss-user eckardm --ss-api-key 4855ff24aae0eafe2c3c29f7bd523d1a1280cdde --transfer-source 6343d913-91dc-4390-881e-35cbdc08d4ff --config-file /usr/lib/archivematica/automation-tools/etc/transfers_bhl-digitalarchive.conf --transfer-type 'unzipped bag' -v
+/usr/share/python/automation-tools/bin/python -m transfers.transfer --user eckardm --api-key 11134b7a2121b3938e2d88cb877a0ed57a2fc004 --ss-user eckardm --ss-api-key 21ed9db78bed720de210e1ac5b43845103957b93 --transfer-source cb1efd8e-9267-4528-b195-deca43550dca --config-file /usr/lib/archivematica/automation-tools/etc/transfers_bhl-digitalarchive.conf --transfer-type 'unzipped bag' -v
